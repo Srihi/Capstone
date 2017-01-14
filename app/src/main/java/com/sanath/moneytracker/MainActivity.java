ede9359a4a5081495a1d39f49ce5832760dc4372
Sanath Nandasiri <sanathe06@gmail.com>
2017-01-14 11:47:56 +0530

    create fragment navigation and option menu for transaction view

--- app/src/main/java/com/sanath/moneytracker/MainActivity.java
+++ app/src/main/java/com/sanath/moneytracker/MainActivity.java
@@ -2,16 +2,28 @@ package com.sanath.moneytracker;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.sanath.moneytracker.ui.fragments.AccountsFragment;
+import com.sanath.moneytracker.ui.fragments.CategoriesFragment;
+import com.sanath.moneytracker.ui.fragments.TransactionsFragment;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private NavigationView navigationView;
+    private DrawerLayout drawer;
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +32,15 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        replaceFragment(TransactionsFragment.newInstance());
     }
 
     @Override
@@ -66,10 +79,43 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
+        final int id = item.getItemId();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Fragment fragment = null;
+                            switch (id) {
+                                case R.id.nav_transactions:
+                                    fragment = TransactionsFragment.newInstance();
+                                    break;
+                                case R.id.nav_accounts:
+                                    fragment = AccountsFragment.newInstance();
+                                    break;
+                                case R.id.nav_categories:
+                                    fragment = CategoriesFragment.newInstance();
+                                    break;
+                            }
+                            replaceFragment(fragment);
+                        } catch (Exception e) {
+                            Log.e(TAG, "Fragment can not be instantiated ", e);
+                        }
+                    }
+                });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            }
+        }).start();
+
+
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_main, fragment).commit();
     }
 }
